95a9ab9e1e94c3aa9bcc971300580232eaac9fab
mifanxiaobai <fhqscfss1013@163.com>
2018-12-07 00:43:50 +0800

    测试Demo

---
+++ src/main/java/com/imooc/activiti/helloworld/DemoMain.java
@@ -0,0 +1,118 @@
+package com.imooc.activiti.helloworld;
+
+import com.google.common.collect.Maps;
+import org.activiti.engine.*;
+import org.activiti.engine.form.FormProperty;
+import org.activiti.engine.form.TaskFormData;
+import org.activiti.engine.impl.form.StringFormType;
+import org.activiti.engine.repository.Deployment;
+import org.activiti.engine.repository.DeploymentBuilder;
+import org.activiti.engine.repository.ProcessDefinition;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.task.Task;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ * 启动类，测试activiti
+ * @author: Mifanxiaobai
+ * @create: 2018-12-06 23:22
+ **/
+public class DemoMain {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DemoMain.class);
+
+    public static void main(String[] args) throws ParseException {
+        LOGGER.info("程序启动成功");
+        //创建流程引擎
+        ProcessEngine processEngine = getProcessEngine();
+
+        //部署流程定义文件
+        String processDefinitionId = deployment(processEngine);
+
+        //启动运行流程
+        //获取运行时对像
+        ProcessInstance processInstance = getProcessInstance(processEngine, processDefinitionId);
+
+        //处理流程任务
+        Scanner scanner = new Scanner(System.in);
+        while (processInstance != null && !processInstance.isEnded()){
+            TaskService taskService = processEngine.getTaskService();
+            List<Task> list = taskService.createTaskQuery().list();
+            LOGGER.info("待处理任务数为【{}】",list.size());
+            for (Task task:list) {
+                LOGGER.info("待处理的任务【{}】",task.getName());
+                FormService formService = processEngine.getFormService();
+                TaskFormData taskFormData = formService.getTaskFormData(task.getId());
+                List<FormProperty> formProperties = taskFormData.getFormProperties();
+                String line = null;
+                Map<String,Object> variables = Maps.newHashMap();
+                for (FormProperty property : formProperties){
+                    //判断用户输入类型
+                    if(StringFormType.class.isInstance(property.getType())){
+                        LOGGER.info("请输入【{}】?",property.getName());
+                        line = scanner.nextLine();
+                        LOGGER.info("您输入的内容是【{}】",line);
+                        variables.put(property.getId(),line);
+                    }else{
+                        LOGGER.info("请输入时间，格式为yyyy-MM-dd【{}】?",property.getName());
+                        line = scanner.nextLine();
+                        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+                        Date parse = sdf.parse(line);
+                        LOGGER.info("您输入的内容是【{}】",line);
+                        variables.put(property.getId(),parse);
+                    }
+                }
+                //提交表单
+                taskService.complete(task.getId(),variables);
+                //提交后更新流程实例
+                processInstance = processEngine.getRuntimeService()
+                        .createProcessInstanceQuery()
+                        .processInstanceId(processInstance.getId())
+                        .singleResult();
+            }
+        }
+        scanner.close();
+        LOGGER.info("程序结束");
+    }
+
+    private static ProcessInstance getProcessInstance(ProcessEngine processEngine, String processDefinitionId) {
+        RuntimeService runtimeService = processEngine.getRuntimeService();
+        ProcessInstance processInstance = runtimeService.startProcessInstanceById(processDefinitionId);
+        LOGGER.info("启动流程【{}】",processInstance.getProcessDefinitionKey());
+        return processInstance;
+    }
+
+    private static String deployment(ProcessEngine processEngine) {
+        RepositoryService repositoryService = processEngine.getRepositoryService();
+        DeploymentBuilder deploymentBuilder = repositoryService.createDeployment();
+        deploymentBuilder.addClasspathResource("activiti-status.bpmn20.xml");
+        Deployment deployment = deploymentBuilder.deploy();
+        String id = deployment.getId();
+
+        //获取流程定义对像
+        ProcessDefinition processDefinition = repositoryService.createProcessDefinitionQuery()
+                .deploymentId(id)
+                .singleResult();
+        String processDefinitionId = processDefinition.getId();
+        String processDefinitionName = processDefinition.getName();
+        LOGGER.info("流程定义名称为【{}】,id为【{}】",processDefinitionName,processDefinitionId);
+        return processDefinitionId;
+    }
+
+    private static ProcessEngine getProcessEngine() {
+        ProcessEngineConfiguration cfg = ProcessEngineConfiguration.createStandaloneInMemProcessEngineConfiguration();
+        ProcessEngine processEngine = cfg.buildProcessEngine();
+        String name = processEngine.getName();
+        String version = processEngine.VERSION;
+        LOGGER.info("流程引擎名称【{}】,版本为【{}】",name,version);
+        return processEngine;
+    }
+}
